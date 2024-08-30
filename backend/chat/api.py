b3595432091440659a7229d53f7d4d911243cc4a
Michael Elliott <mielliott@ufl.edu>
2024-08-30 00:20:15 +0000

    Add more help text

--- backend/chat/api.py
+++ backend/chat/api.py
@@ -69,11 +69,11 @@ def _get_baked_response(agent, history, user_message) -> Iterator[Message]:
                 "This is a prototype chatbot that has some capabilities for retrieving information from iDigBio APIs. "
                 "If you'd like to provide feedback or are looking for more information, you can reach out to the "
                 "developers at https://github.com/acislab/idigbio-chat/issues. Here are some examples of what it can "
-                "do:\n\n" \
-                "* \"How many records does iDigBio have for occurrences in Canada?\""
-                "* \"Find records of *Acer saccharum* that have images in iDigBio\""
-                "* \"Show a map of *Ursus arctos* occurrences\""
-                "* \"What species has the most reported occurrences in Okinawa, Japan?\""
+                "do:\n\n"
+                "\n* \"How many records does iDigBio have for occurrences in Canada?\""
+                "\n* \"Find records of *Acer saccharum* that have images in iDigBio\""
+                "\n* \"Show a map of *Ursus arctos* occurrences\""
+                "\n* \"What species has the most reported occurrences in Okinawa, Japan?\""
             )
         case _:
             pass
