c27df1dabdf7fe77029e5e420f129c0caa9497fe
Michael Elliott <mielliott@ufl.edu>
2024-10-11 20:11:55 +0000

    Don't quote lists encoded in URLs

--- backend/idigbio_util.py
+++ backend/idigbio_util.py
@@ -1,6 +1,8 @@
 def url_encode_inner(x):
     if type(x) == dict:
         return "{" + ",".join([f'"{k}":{url_encode_inner(v)}' for k, v in x.items()]) + "}"
+    elif type(x) == list:
+        return "[" + ",".join([url_encode_inner(v) for v in x]) + "]"
     elif type(x) == str:
         return f'"{x}"'
     elif type(x) == int:
--- tests/test_idigbio_util.py
+++ tests/test_idigbio_util.py
@@ -5,3 +5,9 @@ def test_quote():
     d = {"rq": {"name": "bob", "amount": 3.5}, "limit": 5}
     s = url_encode_params(d)
     assert s == 'rq={"name":"bob","amount":3.5}&limit=5'
+
+
+def test_encode_list():
+    d = {"a_list": [1, 2]}
+    s = url_encode_params(d)
+    assert s == "a_list=[1,2]"
